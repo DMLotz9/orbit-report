636c11e59dadcb6b5f7df2fbeac87716a39e4c77
Dylan Lotz <dschnake09@yahoo.com>
2020-12-14 18:39:44 -0600

    finished project & bonus 1

--- orbit-report/src/app/app.module.ts
+++ orbit-report/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { OrbitReportComponent } from './orbit-report/orbit-report.component';
 import { OrbitListComponent } from './orbit-list/orbit-list.component';
+import { OrbitCountsComponent } from './orbit-counts/orbit-counts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrbitReportComponent,
-    OrbitListComponent
+    OrbitListComponent,
+    OrbitCountsComponent
   ],
   imports: [
     BrowserModule
